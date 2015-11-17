459067a78356b95f68f923ca5b053a63039d5f71
NEIU Developer <neiudeveloper@neiu-stu-lwh056-153.neiu.edu>
2015-11-17 13:48:42 -0600

    Initial Commit

---
+++ Julio Carrera Temp Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Julio Carrera Temp Converter
+//
+//  Created by NEIU Developer on 11/17/15.
+//  Copyright (c) 2015 NEIU Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Julio Carrera Temp ConverterTests/Julio_Carrera_Temp_ConverterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Julio_Carrera_Temp_ConverterTests.swift
+//  Julio Carrera Temp ConverterTests
+//
+//  Created by NEIU Developer on 11/17/15.
+//  Copyright (c) 2015 NEIU Developer. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Julio_Carrera_Temp_ConverterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
